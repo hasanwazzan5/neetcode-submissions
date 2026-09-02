baa9584805eb440b7582a083c7d7722260c55464
hh <hhmingo5@gmail.com>
2026-09-02 21:50:06 +0300

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = list(s)
+        for char in s.copy():
+            letterNum = ord(char) - ord('a')
+            if letterNum < 0 or letterNum > 25:
+                if char not in list('0123456789'):
+                    s.remove(char)
+
+        for i in range(len(s)):
+            if len(s) <= 1:
+                return True
+
+            j = (len(s) - 1) - i
+
+            if i>j:
+                return True
+            elif s[i] != s[j]:
+                return False
+        
+        return True
